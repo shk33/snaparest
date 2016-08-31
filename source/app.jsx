c0d6f275ba39771688292b153202b4648434539c
Miguel Coronel <m.coronel.seg@gmail.com>
2016-08-31 02:23:42 -0500

    Create main react componenet and add snapkite client stream client

--- source/app.jsx
+++ source/app.jsx
@@ -1,31 +1,6 @@
-var React = require('react');
+var React    = require('react');
 var ReactDOM = require('react-dom');
-var ReactClass = React.createClass({
-  getInitialState() {
-      return {
-         isHeaderHidden: false
-      };
-  },
-  handleClick() {
-    this.setState({
-      isHeaderHidden: !this.state.isHeaderHidden
-    });
-  },
-  render() {
-    var title = 'React Stateful component'
-    var headerElement = React.createElement('h1', {className: 'header', key: 'header'}, 
-      title);
+var Application = require('./components/Application.react');
 
-    var buttonElement = React.createElement('button', 
-        {className: 'btn btn-default', onClick: this.handleClick, key: 'button'}, 
-        'Toggle Header');
+ReactDOM.render(<Application />, document.getElementById('react-application'))
 
-    if (this.state.isHeaderHidden) return React.createElement('div', null, [buttonElement]);
-
-    return React.createElement('div', null, [buttonElement, headerElement]);
-  }
-});
-
-var reactComponentElement = React.createElement(ReactClass);
-var reactComponent = ReactDOM.render(reactComponentElement, 
-    document.getElementById('react-application'));
