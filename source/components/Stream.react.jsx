0b9c3c4f3c4654e1a74b106698ace55c1e391247
Miguel Coronel <m.coronel.seg@gmail.com>
2016-09-19 02:12:37 -0500

    Make Stream Component use FLUX arch

--- source/components/Stream.react.jsx
+++ source/components/Stream.react.jsx
@@ -1,42 +1,35 @@
 var React = require('react');
-var SnapkiteStreamClient = require('snapkite-stream-client');
-var StreamTweet = require('./StreamTweet.react.jsx');
-var Header = require('./Header.react.jsx');
+var StreamTweet = require('./StreamTweet.react');
+var Header = require('./Header.react');
+var TweetStore = require('../stores/TweetStore');
 
 var Stream = React.createClass({
   getInitialState: function () {
     return {
-      tweet: null
+      tweet: TweetStore.getTweet()
     }
   },
-
   componentDidMount: function () {
-    SnapkiteStreamClient.initializeStream(this.handleNewTweet);
+    TweetStore.addChangeListener(this.onTweetChange);
   },
-
   componentWillUnmount: function () {
-    SnapkiteStreamClient.destroyStream();
+    TweetStore.removeChangeListener(this.onTweetChange);
   },
-
-  handleNewTweet: function (tweet) {
-    this.setState({
-      tweet: tweet
-    });
-  },
-
   render: function () {
     var tweet = this.state.tweet;
     if (tweet) {
       return (
-        <StreamTweet
-          tweet={tweet}
-          onAddTweetToCollection={this.props.onAddTweetToCollection} />
-        );
+        <StreamTweet tweet={tweet} />
+      );
     }
     return (
       <Header text="Waiting for public photos from Twitter..." />
       );
-  }
+  },
+  onTweetChange: function () {
+    this.setState({
+      tweet: TweetStore.getTweet()
+    });
+  },
 });
-
 module.exports = Stream;
